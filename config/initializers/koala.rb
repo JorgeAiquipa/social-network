3e40289884c2abcbd265be882f670f379f743253
Carlos Duclos <darkzeratul64@gmail.com>
2012-10-01 02:16:17 -0500

    changing koala

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -1,16 +1,9 @@
-module Facebook
-  CONFIG = YAML.load_file(Rails.root.join("config/facebook.yml"))[Rails.env]
-  APP_ID = CONFIG['app_id']
-  SECRET = CONFIG['secret_key']
-  CALLBACK_URL = CONFIG['callback_url']
-end
-
 Koala::Facebook::OAuth.class_eval do
   def initialize_with_default_settings(*args)
     case args.size
     when 0, 1
-      raise 'application id and/or secret are not specified in the config' unless Facebook::APP_ID && Facebook::SECRET
-      initialize_without_default_settings(Facebook::APP_ID.to_s, Facebook::SECRET.to_s, args.first)
+      raise 'application id and/or secret are not specified in the config' unless ENV['FACEBOOK_APP_ID'] && ENV['FACEBOOK_APP_ID']
+      initialize_without_default_settings(ENV['FACEBOOK_APP_ID'].to_s, ENV['FACEBOOK_APP_SECRET'].to_s, args.first)
     when 2, 3
       initialize_without_default_settings(*args)
     end
